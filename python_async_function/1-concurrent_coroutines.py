febf63b8a26dd6427ee42e4a0ccb2ed700dbfba3
Patricio-Benglian <6305@holbertonstudents.com>
2023-08-29 12:34:50 -0700

    Idk why it isnt in order if it should execute in order lol

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,7 @@ import typing
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> typing.List:
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     """returns list of length n with delays up to max_delay"""
     list_times = []
     for i in range(n):
