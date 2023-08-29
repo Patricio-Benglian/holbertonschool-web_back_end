c5941492066479e8b4f704e0742db305de9ba81e
Patricio-Benglian <6305@holbertonstudents.com>
2023-08-29 11:58:40 -0700

    pycode

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -7,7 +7,7 @@ from random import uniform
 import typing
 
 
-async def wait_random(max_delay: int=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """waits random delay between 0 and max_delay"""
     delay = uniform(0, max_delay)
     await asyncio.sleep(delay)
