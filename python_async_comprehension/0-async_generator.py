192952eca8a92f6a969bfe158af9499d05d2104a
Patricio-Benglian <6305@holbertonstudents.com>
2023-08-30 04:30:27 -0700

    Now executes in 1 second instead of 10

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,8 +7,15 @@ from random import uniform
 from typing import Generator
 
 
+async def randgen() -> float:
+    """generates float from 0-10"""
+    await asyncio.sleep(1)
+    return uniform(0, 10)
+
+
 async def async_generator() -> Generator[float, None, None]:
-    """waits 1 second then yields random number between 0 and 10"""
-    for iter in range(10):
-        await asyncio.sleep(1)
-        yield uniform(0, 10)
+    """test"""
+    executions = [randgen(), randgen(), randgen(), randgen(), randgen(),
+                  randgen(), randgen(), randgen(), randgen(), randgen()]
+    for yielded_num in asyncio.as_completed(executions):
+        yield await yielded_num
