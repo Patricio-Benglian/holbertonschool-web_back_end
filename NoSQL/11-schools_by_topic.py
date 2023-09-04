edb8cd9ff2e9172e4cd4dc9b3b3c2a1afb1880a3
Patricio-Benglian <6305@holbertonstudents.com>
2023-09-04 06:49:52 -0700

    find

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""returns lsit of schools with specific topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """refer to module doc"""
+    mongo_collection.find({"topics": topic})
