c456dd8fa8acf8c5798c467cda725629e9537975
Patricio-Benglian <6305@holbertonstudents.com>
2023-09-15 06:28:50 -0700

    does this work idk haha

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,50 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof (name) !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof (length) !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw TypeError('Students must be an array of strings');
+    }
+    if (!students.every((element) => typeof (element) === 'string')) {
+      throw TypeError('Students must be an array of strings');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(name) {
+    if (typeof (name) !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  set length(length) {
+    if (typeof (length) !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  set students(students) {
+    this._students = students;
+  }
+
+  // you cannot use arrow things for getters apparently
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
