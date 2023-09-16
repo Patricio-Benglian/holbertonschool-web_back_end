eb4eafeb3ea7d95b288885b0f9be46d50456ad16
Patricio-Benglian <6305@holbertonstudents.com>
2023-09-16 10:26:23 -0700

    Cast to string and number

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,30 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  set size(size) {
+    this._size = size;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set location(location) {
+    this._location = location;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  toString() {
+    return this._location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+}
